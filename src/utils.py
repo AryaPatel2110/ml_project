4e67853299257a49664dc0e22d1387915a946f8d
AryaPatel2110 <patelarya899@gecg28.ac.in>
2024-04-11 11:48:41 +0530

    Web App

--- src/utils.py
+++ src/utils.py
@@ -54,6 +54,16 @@ def model_report(y_pred, y_test, file_path):
             
     except Exception as e:
         raise CustomException(e,sys)
+    
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
+    
 
 
 
