b7c000a3e7ff854edf12a4ab119f5091c40d6c1d
AryaPatel2110 <patelarya899@gecg28.ac.in>
2024-04-14 22:25:54 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,6 +22,6 @@ version='0.0.1',
 author='Arya',
 author_email='patelarya899@gmail.com',
 packages=find_packages(),
-install_requires = get_requirnments('requirnments.txt')
+install_requires = get_requirnments('requirements.txt')
 )
 
